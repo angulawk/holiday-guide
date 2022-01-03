763e73453cbe0e6b4556d6aba909fdf142859532
Agnieszka Wójcik <agnieszka.weronika.wojcik@gmail.com>
2022-01-03 19:00:16 +0100

    Create react app, add typescript and eslint

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import { CountriesContainer } from "containers/CountriesContainer";
+
+function App() {
+  return (
+    <Fragment>
+      <CountriesContainer />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+import { colorPalette } from "styles/variables/colorPalette";
+
+const theme: DefaultTheme = {
+  colorPalette
+};
+
+export { theme };
